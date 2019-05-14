920d76d3790f9f6badc64d75cb5ca2c54b12cfb9
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-05-14 16:47:37 +0200

    Updated tests

--- test/PasswordUtilsTest.java
+++ test/PasswordUtilsTest.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordUtilsTest {
+
+    @Test
+
+    void test1() {
+        String salt = PasswordUtils.generateSalt();
+        String pwd = "password";
+        String pwd_hash = PasswordUtils.hashPassword(pwd, salt);
+        assertTrue(PasswordUtils.verifyPassword(pwd,pwd_hash,salt));
+    }
+
+    @Test
+    void test2() {
+        String salt = PasswordUtils.generateSalt();
+        String pwd = "password";
+        String pwd_hash = PasswordUtils.hashPassword(pwd, salt);
+        assertFalse(PasswordUtils.verifyPassword("ads",pwd_hash,salt));
+    }
 
 }
